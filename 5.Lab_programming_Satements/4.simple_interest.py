278e91673582af926e07b251e3d67cec083c9743
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-17 06:54:26 +0530

    Add files via upload

---
+++ 5.Lab_programming_Satements/4.simple_interest.py
@@ -0,0 +1,12 @@
+# 4. Find the Simple Interest on Rs. 200 for 5 years at 5% per year.
+
+# Given values are as follows:
+principal = 200  # as in Rs.
+rate_of_interest = 5  # as in %
+time = 5  # as in years
+
+# formula for calculating Simple Interest is:
+simple_interest = (principal * rate_of_interest * time) / 100
+
+# printing the result
+print(f"The Simple Interest on Rs. {principal} for {time} years at {rate_of_interest}% per year is Rs. {simple_interest}.")
