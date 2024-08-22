7359c5a8921923a4cf2dcbb5427d367ecef9ef60
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-22 06:11:07 +0530

    Add files via upload

---
+++ 7.Lab_Functions2/1.div_function.py
@@ -0,0 +1,11 @@
+# 1. Declare a div() function with two parameters. Then call the function and pass two numbers and display their division. 
+
+#defining the function
+def div(a, b):
+    return a / b
+
+# Calliing the function with two numbers
+result = div(10, 2)
+
+#printing the result
+print("Division Result:", result)
