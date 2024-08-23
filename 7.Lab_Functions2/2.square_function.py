825822d78f3dbbe5191eefed139105e12da7dac8
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-23 16:24:43 +0530

    Add files via upload

---
+++ 7.Lab_Functions2/2.square_function.py
@@ -0,0 +1,11 @@
+# 2. Declare a square() function with one parameter.Then call the function and pass one number and display the square of that number . 
+
+#definig the function square()
+def square(x):
+    return x * x
+
+# Calling the function with one number
+result = square(5)
+
+#printing the result
+print("Square Result:", result)
