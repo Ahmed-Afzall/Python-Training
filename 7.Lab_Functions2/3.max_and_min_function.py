5b5cd196c3d4d6b6987925ee32b2dfae97894e91
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-23 16:25:20 +0530

    Add files via upload

---
+++ 7.Lab_Functions2/3.max_and_min_function.py
@@ -0,0 +1,13 @@
+# 3. Using max() and min() functions display the maximum and minimum of 5 random numbers. 
+
+# I have manually provided 5 random numbers in a list
+numbers = [10, 25, 36, 47, 5]
+
+# Finding the maximum and minimum using max() and min() function
+maximum = max(numbers)
+minimum = min(numbers)
+
+#printing the max and min numbers from the list
+print("Numbers:", numbers)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
