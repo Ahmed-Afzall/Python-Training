212beaee5aee0ee2e29c64a32fcf22e8c3882473
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-24 18:52:18 +0530

    Add files via upload

---
+++ 7.Lab_Functions2/4.lower_funtion.py
@@ -0,0 +1,8 @@
+# taking a name from the user as an input
+name = input("Enter your name: ")
+
+# Converting to lower case
+lower_case_name = name.lower()
+
+#printing the lower cased name
+print("Name in Lower Case:", lower_case_name)
