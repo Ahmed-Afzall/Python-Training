515ec13e6c22efaf79450f1a7fe05fac7ab65579
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-25 10:31:11 +0530

    Add files via upload

---
+++ 8.Lab_For_Loop/1.table_of_5.py
@@ -0,0 +1,6 @@
+# 1. Print the table of 5 using for loop
+
+# in range, 1 is the starting value and 11 is the stop value
+# 11 is stop value so that loop can terminate at 10
+for i in range(1, 11):
+    print(f"5 x {i} = {5 * i}") # this will print like - (5 x i = 5*i) i is the value from 1 to 10
