ab61d92322233c19d69f3b49cb8c855bd5ef0c12
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-26 19:05:51 +0530

    Add files via upload

---
+++ 8.Lab_For_Loop/3.list_and_for_loop.py
@@ -0,0 +1,8 @@
+# 3. Create a list and iterate through its items using a for loop:
+
+# Creating a list
+fruits = ["apple", "banana", "mango", "blueberry"]
+
+# Iterating through the items of the list
+for item in fruits:
+    print(item)
