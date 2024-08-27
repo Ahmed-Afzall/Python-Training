cbd68ea5035792b25ccde47cad1840e2ae1e36a4
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-27 07:01:08 +0530

    Add files via upload

---
+++ 8.Lab_For_Loop/6.natural_numbers.py
@@ -0,0 +1,18 @@
+# 6. Print the first 10 natural numbers using for loop 
+# start value is 1 and stop value 11 so that loop can terminate at 10
+for i in range(1, 11):
+    print(i)
+
+'''
+output = 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+'''
