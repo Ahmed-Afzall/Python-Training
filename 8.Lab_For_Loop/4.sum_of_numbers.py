ebe014ccf41117834c592dda963033c84d3e9346
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-08-27 06:59:51 +0530

    Add files via upload

---
+++ 8.Lab_For_Loop/4.sum_of_numbers.py
@@ -0,0 +1,9 @@
+#4. Calculate the sum of numbers from 1 to 10 
+
+sum_total = 0 #the initial value is 0
+for i in range(1, 11): #start from 1 and stop at 10
+    sum_total += i #adding and assigning the numbers from 1 to 10
+#printing the sum
+print("Sum of numbers from 1 to 10 is:", sum_total)
+
+#output = 55
