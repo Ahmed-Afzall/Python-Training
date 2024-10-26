a9142daf84c50929cf7482fdc7c138eb4c9047e2
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-10-26 19:24:50 +0530

    Add files via upload

---
+++ 14.Lab_List/1.summing_items_in_list.py
@@ -0,0 +1,12 @@
+# Defining a list
+list = [1, 2, 3, 4, 5]
+
+# Initializing a variable to store the sum (currently 0)
+total = 0
+
+# Looping through each item in the list through for loop
+for item in list:
+    total += item  # Adding each item to total
+
+# Printing the result
+print("Sum of the list:", total)
