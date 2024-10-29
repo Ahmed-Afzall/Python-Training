586f0f934666a7587d0a3e54d04eb2f54466adf3
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-10-29 12:22:00 +0530

    Add files via upload

---
+++ 14.Lab_List/3.finding_duplicates.py
@@ -0,0 +1,14 @@
+# Defining a list
+list = [1, 2, 2, 3, 4, 4, 5, 1]
+
+# Creating an empty list to store duplicates
+duplicates = []
+
+# Looping through the list
+for item in list:
+    # Checking if the item appears more than once and is not already in duplicates list
+    if list.count(item) > 1 and item not in duplicates:
+        duplicates.append(item) # this will append/add the item in duplicate list
+
+# Printing the duplicates
+print("Duplicate values:", duplicates)
