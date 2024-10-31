e103f1ef5416a5d4374698ad3de8a11d30b0cdcb
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-10-31 10:21:39 +0530

    Add files via upload

---
+++ 14.Lab_List/5.traversing_a_list_in_reverse.py
@@ -0,0 +1,6 @@
+# Defining a list
+list = ['red', 'green', 'white', 'black']
+
+# Looping through the list in reverse using a range
+for i in range(len(list)-1, -1, -1):
+    print(f"Element: {list[i]}, Original Index: {i}") #printing the reversed elements with their original index
