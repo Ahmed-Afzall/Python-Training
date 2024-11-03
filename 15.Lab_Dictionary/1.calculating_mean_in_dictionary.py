20d75de1d08216e987409d1f3a6d6c606df83fec
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2024-11-03 23:22:59 +0530

    Add files via upload

---
+++ 15.Lab_Dictionary/1.calculating_mean_in_dictionary.py
@@ -0,0 +1,11 @@
+# Defining the dictionary
+dict = {"A": 6, "B": 9, "C": 5, "D": 7, "E": 4}
+
+# Getting all the values from the dictionary
+values = dict.values()
+
+# Calculating the mean (sum of values divided by number of values)
+mean = sum(values) / len(values)
+
+# Printing the result
+print("Mean:", mean)
