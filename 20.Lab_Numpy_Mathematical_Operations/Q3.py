5a3a22d4d1303337b920c2def9188ecca4c0f781
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2025-01-17 18:30:30 +0530

    Add files via upload

---
+++ 20.Lab_Numpy_Mathematical_Operations/Q3.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+inventory = np.array([10, 0, 5, 0, 20, 0])
+
+out_of_stock_indices = np.where(inventory == 0)
+out_of_stock_products = inventory[out_of_stock_indices]
+
+print("Out of Stock Products:", out_of_stock_products)
