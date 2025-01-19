ed43ffc3f1d33b4868a99ade86478abed891664d
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2025-01-20 00:13:41 +0530

    Update Q4.py

--- 20.Lab_Numpy_Mathematical_Operations/Q4.py
+++ 20.Lab_Numpy_Mathematical_Operations/Q4.py
@@ -6,3 +6,12 @@ price_per_item = np.array([10.0, 5.0, 8.0, 12.0])
 total_cost = quantity * price_per_item
 
 print("Total Cost of Items:", total_cost)
+
+-------------------------------
+# Create 1D arrays
+array1 = np.array([1, 2, 3, 4, 5])
+array2 = np.array([5, 4, 3, 2, 1])
+
+# Create 2D arrays
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
