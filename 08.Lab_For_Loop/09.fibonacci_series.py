58321a89dd2563ceeafcafbad0da65edfd28d759
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2025-12-17 07:40:04 +0530

    Update 09.fibonacci_series.py

--- 08.Lab_For_Loop/09.fibonacci_series.py
+++ 08.Lab_For_Loop/09.fibonacci_series.py
@@ -5,3 +5,4 @@ a, b = 0, 1              # Initializing the first two numbers of the Fibonacci s
 while a <= 50:           # Loop to print Fibonacci numbers until 'a' is less than or equal to 50
     print(a, end=" ")    # Printing the current value of 'a'
     a, b = b, a + b      # Update the values of 'a' and 'b. The new 'a' becomes the old 'b', and the new 'b' becomes the sum of old 'a' and 'b'
+
