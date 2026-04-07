5d205ad83ea28640521f224c977a893cfbd6439c
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2026-04-07 21:11:25 +0530

    Update 3.accessing_key_value_item.py

--- 15.Lab_Dictionary/3.accessing_key_value_item.py
+++ 15.Lab_Dictionary/3.accessing_key_value_item.py
@@ -4,4 +4,3 @@ dict_num = {"A": 10, "B": 20, "C": 30, "D": 40, "E": 50, "F": 60}
 # Looping through the dictionary to get key, value, and item
 for key, value in dict_num.items():
     print(f"Key: {key}, Value: {value}, Item: ({key}, {value})")
-
