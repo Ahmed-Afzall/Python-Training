4cd2d3b9e2f76dc2a891381af5fb4a4388a6c4d0
Afzal <157731942+Ahmed-Afzall@users.noreply.github.com>
2026-08-30 10:14:52 +0530

    Update 07.removing_occurrences.py

--- 11.Lab_String_Questions/07.removing_occurrences.py
+++ 11.Lab_String_Questions/07.removing_occurrences.py
@@ -12,4 +12,4 @@ def remove_character(input_string, char_to_remove):
 user_input = input("Enter a string: ")
 char_to_remove = input("Enter the character to remove: ")
 output = remove_character(user_input, char_to_remove)
-print("New string:", output)
+print("New string:", output)
